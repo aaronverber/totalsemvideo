fa8f80358859be593fe084b4dc40d489e10ff33a
Nathan Hartman <nathanial.hartman@gmail.com>
2012-08-27 00:05:36 -0500

    updated video urls

--- app/helpers/video_helper.rb
+++ app/helpers/video_helper.rb
@@ -1,2 +1,5 @@
 module VideoHelper
+  def flashify(url)
+    url.gsub(':','%3A').gsub('/','%2F').gsub(' ', '%2B')
+  end
 end
--- data/load_data.rb
+++ data/load_data.rb
@@ -7,16 +7,24 @@ u.save
 
 course = Course.find_or_create_by_name("A+")
 course.save
-File.open("episodes and chapters.csv").each_line do |line|
+File.open("data/episodes and chapters.csv").each_line do |line|
   cnum,cname,enum,ename = line.split(',').find_all{|x| x =~ /\w/}
   if !cname.nil? && !cname.empty?
     chapter = Chapter.find_or_create_by_name(cname)
     chapter.name = cname
     chapter.number = cnum
     chapter.course = course
+
+    escaped_name = ename.gsub(' ', '+').gsub('!',',').chop
+    video_url = "https://s3.amazonaws.com/totalsem-videos/%03d #{escaped_name}.mp4" % enum
+
+    puts line
+    puts video_url
     
-    episode = Episode.new(name: ename, number: enum, chapter: chapter)
-    puts chapter.name
+    episode = Episode.new(name: ename,
+                          number: enum, 
+                          chapter: chapter, 
+                          video_url: video_url)
     chapter.save
     episode.save
   end

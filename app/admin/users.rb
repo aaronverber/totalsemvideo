25c27de1850e93faf223d996baf4bc7ade6f95cd
Nathan Hartman <nathanial.hartman@gmail.com>
2012-09-17 22:16:46 -0500

    fixed email issue

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register User do
   form do |f|
     f.inputs "User Information" do
+      f.input :login
       f.input :email
       f.input :password
       f.input :password_confirmation
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
+  acts_as_authentic do |config|
+    config.validate_email_field = false
+  end
+
   attr_accessible :login, :email, :password, :password_confirmation
   has_many :watcheds
   has_many :videos, :through => :watcheds
+  
+  validates :email, :uniqueness => true, :allow_nil => true
 
   def self.find_by_anything(login)
     find_by_login(login) || find_by_email(login) || find_by_id(login)

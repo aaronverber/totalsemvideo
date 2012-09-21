4e90793a439c60f3c654a349e1eae08e76be38ae
Nathan Hartman <nathanial.hartman@gmail.com>
2012-09-21 01:58:58 -0500

    removed cookies

--- public/scripts/adminica/adminica_load.js
+++ public/scripts/adminica/adminica_load.js
@@ -9,44 +9,6 @@ $(document).ready(function() {
 	adminicaGallery();
 	adminicaWizard();
 	adminicaVarious();
-
-
-	$("a.pjax.pjax_on").pjax("body",{
-		fragment:"#pjax",
-		timeout:"5000",
-		beforeSend: function(){
-			showLoadingOverlay();
-		},
-		complete: function(){
-			hideLoadingOverlay();
-		},
-		success: function(){
-			adminicaUi();
-			adminicaForms();
-			adminicaMobile();
-			adminicaDataTables();
-			adminicaCalendar();
-			adminicaCharts();
-			adminicaGallery();
-			adminicaWizard();
-			adminicaVarious();
-			pjaxToggle();
-		},
-		error: function(){
-		}
-	});
-
-	pjaxToggle();
-
-	$('#pjax_switch #dynamic_on').on("change",function() {
-		$("a.pjax").addClass("pjax_on");
-		$.cookie('pjax_on', true);
-	});
-
-	$('#pjax_switch #dynamic_off').on("change",function() {
-		$("a.pjax").removeClass("pjax_on");
-		$.cookie('pjax_on', false);
-	});
 });
 
 $(window).load(function(){

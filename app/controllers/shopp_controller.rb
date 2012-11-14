b0ce6881edac341b008a095f708db09a2b8b2c14
Nathan Hartman <nathanial.hartman@gmail.com>
2012-11-14 14:58:12 -0600

    fixed parsing

--- app/controllers/shopp_controller.rb
+++ app/controllers/shopp_controller.rb
@@ -2,40 +2,53 @@ class ShoppController < ApplicationController
   skip_before_filter :require_user, :only => [:add_user, :add_course_access]
 
   def add_user
+    logger.info 'got an add_user request'
     if request.post?
       begin 
-        user_json = ActiveSupport::JSON.decode(request.body)
+        logger.info "received"
+        user_json = decode_raw(params.keys[0])
+        logger.info user_json
         if not user_json.has_key? "name"
-          render :text => "missing user name\n"
+          logger.error "missing user name"
         elsif not user_json.has_key? "password"
-          render :text => "missing user password\n"
+          logger.error "missing user password"
         else
-          render :text => "success: added user\n"
+          logger.info "success: added user"
         end
       rescue
-        render :text => "could not parse json\n"
+        logger.error "could not parse json"
       end
     else
-      render :text => "failure: expected post\n"
+      logger.error "failure: expected post"
     end    
+    render :text => "Ok"
   end
 
   def add_course_access
+    logger.info 'got an add_course request'
     if request.post?
       begin 
-        course_json = ActiveSupport::JSON.decode(request.body)
+        logger.info "received"
+        course_json = decode_raw(params.keys[0])
+        logger.info course_json
         if not course_json.has_key? "course_name"
-          render :text => "missing course_name\n"
+          logger.error "missing course_name"
         elsif not course_json.has_key? "user_name"
-          render :text => "missing user_name\n"
+          logger.error "missing user_name"
         else
-          render :text => "success: added course access\n"
+          logger.info "success: added course access"
         end
       rescue
-        render :text => "could not parse json\n"
+        logger.error "could not parse json"
       end  
     else
-      render :text => "failure: expected post\n"
+      logger.error "expected post"
     end
+    render :text => "Ok"
+  end
+
+  def decode_raw(text)
+    logger.info("decode: " + text)
+    return ActiveSupport::JSON.decode(text)
   end
 end
